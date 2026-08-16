5191e77063cb498b7729ccb131cbc8ae47190a49
Symflex <symeon.chardonnens@wapiti.ch>
2026-08-16 23:26:41 +0200

    manoeuvre mineure

--- app/src/main/java/ch/ecoandco/enfantsDesCopains/MainActivity.kt
+++ app/src/main/java/ch/ecoandco/enfantsDesCopains/MainActivity.kt
@@ -385,6 +385,9 @@ class MainActivity : AppCompatActivity() {
         val layoutSelection = findViewById<View>(R.id.layoutSelection)
         layoutSelection.visibility = if (afficher) View.VISIBLE else View.GONE
 
+        val layoutBouton = findViewById<View>(R.id.boutonAjouterContainer)
+        layoutBouton.visibility = if (afficher) View.GONE else View.VISIBLE
+
         if (afficher) {
             // Bouton Annuler
             findViewById<Button>(R.id.btnAnnulerSelection).setOnClickListener {
