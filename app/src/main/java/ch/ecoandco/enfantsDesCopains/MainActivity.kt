4857fa6a29c812f29b8f9f3443ac39b486a9180d
Symflex <symeon.chardonnens@wapiti.ch>
2026-08-19 22:51:00 +0200

    Version assez stable pour PR et merge

--- app/src/main/java/ch/ecoandco/enfantsDesCopains/MainActivity.kt
+++ app/src/main/java/ch/ecoandco/enfantsDesCopains/MainActivity.kt
@@ -196,7 +196,6 @@ class MainActivity : AppCompatActivity() {
 // Optionnel : Écouter les changements de sélection pour mettre à jour un compteur
             adaptateur.onSelectionChanged = { nombre ->
                 mettreAJourTitreSelection(nombre)
-                afficherToastPersonnalise("Un de plus")
             }
 // Lancement du premier chargement
             chargerDonneesDepuisBDD()
